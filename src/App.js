533293f455144e91a7142513e1efd00097973373
akshayzagad <akshayzagade62@gmail.com.com>
2025-02-23 23:46:01 +0530

    Refactor App component for improved readability and consistency in JSX formatting

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ const messages = [
 ];
 
 export default function App() {
-
-  const [step, setState] = useState(1)
+  const [step, setState] = useState(1);
   const [isOpened, setIsOpened] = useState(true);
 
   function previous() {
@@ -21,31 +20,41 @@ export default function App() {
 
   return (
     <div>
-      <button className="close" onClick={() => setIsOpened(!isOpened)}>x</button>
+      <button className="close" onClick={() => setIsOpened(!isOpened)}>
+        x
+      </button>
       {isOpened && (
         <div className="steps">
           <div className="numbers">
-            <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-            <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-            <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
           <StepMessage step={step}>
-          <div className="buttons">
-            <Button textColor='#fff' bgColor='#7950f2' onClick={() => alert(`Learn how to ${messages[step - 1]}`)}>
-              Learn How
-            </Button>
-          </div>
+            <div className="buttons">
+              <Button
+                textColor="#fff"
+                bgColor="#7950f2"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn How
+              </Button>
+            </div>
           </StepMessage>
 
           <div className="buttons">
-            <Button textColor='#fff' bgColor='#7950f2' onClick={previous}><span>⏮️</span> Previous</Button>
-            <Button textColor='#fff' bgColor='#7950f2' onClick={next}>Next <span>⏭️</span></Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={previous}>
+              <span>⏮️</span> Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={next}>
+              Next <span>⏭️</span>
+            </Button>
             {/* <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={() => next()}>Next</button> */}
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 
 function StepMessage({ step, children }) {
@@ -54,11 +63,16 @@ function StepMessage({ step, children }) {
       <h3>steps {step} </h3>
       {children}
     </div>
-  )
+  );
 }
 
 function Button({ textColor, onClick, bgColor, children }) {
   return (
-    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>{children}</button>
-  )
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
